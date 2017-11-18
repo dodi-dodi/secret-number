629528d200836009c631669b50c5bb9c04f2baf7
dodi-dodi <dorota.durlej@gmail.com>
2017-11-18 12:17:07 +0100

    readability improvement

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
+const path = require('path');
 const express = require('express');
 const app = express();
-const path = require("path");
 
 let secretNumber;
 
 app.use(express.json());
-app.use(express.urlencoded());
 
 app.get('/',function(req, res){
     secretNumber = Math.floor(Math.random() * 10000) + 1;
-    console.log("secret number is: ", secretNumber);
-    res.sendFile(path.join(__dirname + '/index.html'));
+    console.log('secret number is: ', secretNumber);
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.post('/', function(req, res) {
